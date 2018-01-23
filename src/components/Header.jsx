af8fdde98adf31102ebbd9cddd1c00200c5b57dc
bola <bola.ringley.co.uk>
2018-01-23 14:02:55 +0000

    complete the stopwatch

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Stat from './Stat'
+import Stat from './Stat';
+import Stopwatch from './Stopwatch';
 
 const Header = (props) => {
   return(
     <div className="header">
      <Stat totalplayer = { props.initialplayers } />
       <h1> { props.title } </h1>
-        <div className="stopwatch">
-          <p>STOPWATCH</p>
-          <p><span>1</span></p>
-          <button className="stop-btn">STOP</button>
-          <button className="reset-btn">RESET</button>
-        </div>
+        <Stopwatch />
     </div>
   );
 };
