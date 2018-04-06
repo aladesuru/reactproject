26f8f16546fc606ecf10e4487d0ebb98117e5d25
bola <bola.ringley.co.uk>
2018-04-06 18:02:54 +0100

    resrtucturing layout

---
+++ src/components/SourceCode.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Sourcecode = () => {
+	return(
+		<div className="sourcecode">
+			<h2>Application Source Code</h2>
+			<p>
+				<strong>Please visit:</strong>
+			</p>
+			<p> https://github.com/aladesuru/scoreboard-app</p>
+		</div>
+		);
+}
+
+export default Sourcecode;
