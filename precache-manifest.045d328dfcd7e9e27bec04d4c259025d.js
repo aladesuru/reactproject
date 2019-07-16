ddfc5a1c7e729378c7247e5ab6885875b7cd2ba3
Adebola Aladesuru <adebolaaladesuru@Adebolas-MacBook-Pro.local>
2019-07-16 10:48:30 +0100

    Updates

---
+++ precache-manifest.045d328dfcd7e9e27bec04d4c259025d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e9d20e2f03938e28cda67930f3afe95",
+    "url": "/scoreboard-app/index.html"
+  },
+  {
+    "revision": "ddef32b486ffee768034",
+    "url": "/scoreboard-app/static/css/main.48dd010a.chunk.css"
+  },
+  {
+    "revision": "65fdf41dd1b5100eea31",
+    "url": "/scoreboard-app/static/js/2.4e7e4130.chunk.js"
+  },
+  {
+    "revision": "ddef32b486ffee768034",
+    "url": "/scoreboard-app/static/js/main.6d9b214f.chunk.js"
+  },
+  {
+    "revision": "7b201822fa1216f00bb6",
+    "url": "/scoreboard-app/static/js/runtime~main.8f965656.js"
+  }
+]);
